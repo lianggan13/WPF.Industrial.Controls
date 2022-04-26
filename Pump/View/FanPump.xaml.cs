c7af2b53946cfcbb42840f6d2ff8dae40ae725ee
zhangliang <676021630@qq.com>
2022-04-26 09:20:44 +0800

    s

---
+++ Pump/View/FanPump.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Pump.View
+{
+    /// <summary>
+    /// FanPump.xaml 的交互逻辑
+    /// </summary>
+    public partial class FanPump : UserControl
+    {
+        public FanPump()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsRun
+        {
+            get { return (bool)GetValue(IsRunProperty); }
+            set { SetValue(IsRunProperty, value); }
+        }
+        public static readonly DependencyProperty IsRunProperty =
+            DependencyProperty.Register(
+                "IsRun",
+                typeof(bool),
+                typeof(FanPump),
+                new PropertyMetadata(default(bool), new PropertyChangedCallback(OnRunStateChanged)));
+
+        private static void OnRunStateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var v = bool.Parse(e.NewValue.ToString());
+            if (v)
+            {
+                VisualStateManager.GoToState(d as FanPump, "RunState", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(d as FanPump, "Stop", true);
+            }
+        }
+    }
+}
