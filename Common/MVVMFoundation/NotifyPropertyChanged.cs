439aeaafdadfb82c624cd90c4419f2905158c53b
zhangliang <676021630@qq.com>
2022-05-19 15:20:42 +0800

    s

---
+++ Common/MVVMFoundation/NotifyPropertyChanged.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Common.MVVMFoundation
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 属性改变时触发
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
